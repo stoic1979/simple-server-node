3a75ddc63475a3f8198ea7d102a2c413c8ed697f
Navjot Singh <weavebytes@gmail.com>
2018-01-18 15:14:10 +0530

    remove onoff in pkg file

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 var express = require('express');
 var app = express();
 
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out');
-var blinkInterval = setInterval(blinkLED, 250);
+// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+// var LED = new Gpio(4, 'out');
+// var blinkInterval = setInterval(blinkLED, 250);
 
-// reply to request with "Hello World!"
-app.get('/toggle_led/:pin', function (req, res) {
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  }
-  else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
+// // reply to request with "Hello World!"
+// app.get('/toggle_led/:pin', function (req, res) {
+//   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+//     LED.writeSync(1); //set pin state to 1 (turn LED on)
+//   }
+//   else {
+//     LED.writeSync(0); //set pin state to 0 (turn LED off)
+//   }
 
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
+//   clearInterval(blinkInterval); // Stop blink intervals
+//   LED.writeSync(0); // Turn LED off
+//   LED.unexport(); // Unexport GPIO to free resources
 
-  setTimeout(endBlink, 5000);
+//   setTimeout(endBlink, 5000);
 
-});
+// });
 
 // reply to request with "Hello World!"
 app.get('/about', function (req, res) {
