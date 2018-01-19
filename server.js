06c540c7f8089d505b9c0fb8f93a35ebb1b13ee9
Navjot Singh <weavebytes@gmail.com>
2018-01-19 15:14:43 +0530

    roll back

--- server.js
+++ server.js
@@ -1,54 +1,10 @@
 var express = require('express');
 var app = express();
 
-// var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-// var LED = new Gpio(4, 'out');
-// var blinkInterval = setInterval(blinkLED, 250);
-
-// // reply to request with "Hello World!"
-// app.get('/toggle_led/:pin', function (req, res) {
-//   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-//     LED.writeSync(1); //set pin state to 1 (turn LED on)
-//   }
-//   else {
-//     LED.writeSync(0); //set pin state to 0 (turn LED off)
-//   }
-
-//   clearInterval(blinkInterval); // Stop blink intervals
-//   LED.writeSync(0); // Turn LED off
-//   LED.unexport(); // Unexport GPIO to free resources
-
-//   setTimeout(endBlink, 5000);
-
-// });
-// 19 data
-var gpio = require('rpi-gpio');
-gpio.setup(18, gpio.DIR_OUT);
-
-app.get('/on', function(req, res){
-	gpio.write(18, true, function(err) {
-        if (err) res.send("on error: " + err);
-        res.send('Pin 18 on done');
-    });
-});
-
-app.get('/off', function(req, res){
-	gpio.write(18, false, function(err) {
-        if (err) res.send("off error: " + err);
-        res.send('Pin 18 off done');
-    });
-});
-
-// reply to request with "Hello World!"
-app.get('/about', function (req, res) {
-  res.send('<b> This is a resin io test </b>');
-});
-
 // reply to request with "Hello World!"
 app.get('/', function (req, res) {
-  res.send('<b> My first node app with resin.io running on PI 3 </b>');
+  res.send('Hello World!');
 });
-
 
 //start a server on port 80 and log its start to our console
 var server = app.listen(80, function () {
